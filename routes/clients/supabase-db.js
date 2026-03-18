b794df819ee075460dfa68737881c7f2751767de
leider calvo <leiderstivencalvogomez@hotmail.com>
2026-03-18 08:44:39 -0500

    god why

--- routes/clients/supabase-db.js
+++ routes/clients/supabase-db.js
@@ -1,9 +1,9 @@
 import { Pool } from 'pg';
 
 export const pool = new Pool({
-  host: "aws....",
+  host: "aws-1-us-east-1.pooler.supabase.com",
   port: "6543",
   database: "postgres",
-  user: "******",
-  password: "------",
+  user: "postgres.uualhzfvyhfmedsvfmuj",
+  password: "kUklb1vnpEK79zYo",
 });
